5b980053aad751379b4c034994aac0b00f3cc4cf
outhewof <outhewof@gmail.com>
2023-11-30 13:01:13 +0100

    feat: toolbar with menus

--- app/src/main/java/com/outhewof/restimer/MainActivity.java
+++ app/src/main/java/com/outhewof/restimer/MainActivity.java
@@ -1,15 +1,42 @@
-
 package com.outhewof.restimer;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_app_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_settings) {
+            // Handle menu item click
+            return true;
+        } else {
+
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
